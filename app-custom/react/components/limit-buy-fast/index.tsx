8b27614ca25236be0dc35bb7d963616a428baa69
lucasastinofg <lucas.astino@agenciafg.com.br>
2024-09-09 16:28:22 -0300

    ajustando buy fast PDP

--- app-custom/react/components/limit-buy-fast/index.tsx
+++ app-custom/react/components/limit-buy-fast/index.tsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import { useEffect, useState } from "react";
 
 export function limitBuyFast() {
@@ -6,13 +5,12 @@ export function limitBuyFast() {
 
   useEffect(() => {
     const handleScroll = () => {
-      const scrollTop = window.scrollY; // Distância atual do topo
-      const windowHeight = window.innerHeight; // Altura da janela
-      const docHeight = document.documentElement.scrollHeight; // Altura total da página
+      const scrollTop = window.scrollY; 
+      const windowHeight = window.innerHeight;
+      const docHeight = document.documentElement.scrollHeight;
 
-      const remainingScroll = docHeight - (scrollTop + windowHeight); // Quanto falta para o final
+      const remainingScroll = docHeight - (scrollTop + windowHeight);
 
-      // Se a distância até o final for menor que 5% da altura total da página
       if (remainingScroll <= docHeight * 0.05) {
         setIsNearBottom(true);
       } else {
@@ -20,17 +18,14 @@ export function limitBuyFast() {
       }
     };
 
-    // Adiciona o evento de scroll
     window.addEventListener("scroll", handleScroll);
 
-    // Remove o evento de scroll ao desmontar o componente
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   useEffect(() => {
-    // Adiciona a classe 'scrolled-near-bottom' se chegar nos 5% finais
     const element = document.querySelector(
       ".vtex-sticky-layout-0-x-container--buy-fast-pdp"
     );
